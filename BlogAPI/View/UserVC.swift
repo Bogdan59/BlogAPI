4a41d4e3d30863ec850f92a4950bc0bdba15b228
Bogdan Dubiahin <dubiahinb@gmail.com>
2018-03-21 17:04:29 +0200

    LoginScr

--- BlogAPI/View/UserVC.swift
+++ BlogAPI/View/UserVC.swift
@@ -7,112 +7,54 @@
 //
 
 import UIKit
-import Alamofire
+
 
 class UserVC: UIViewController {
 
-    let URLUser = "http://fed-blog.herokuapp.com/api/v1/security/login"
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var passField: UITextField!
+    @IBOutlet weak var loginButt: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
     
-    let defaultValues = UserDefaults.standard
     
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var passTextField: UITextField!
-    @IBOutlet weak var massegeLabel: UILabel!
     
-    @IBAction func loginButton(_ sender: UIButton) {
+    @IBAction func loginTapped(_ sender: UIButton) {
+        let parameters = ["name": self.nameField.text!, "password": self.passField.text! ] as [String : Any]
         
-        let logParameters: Parameters=[
-            "name":nameTextField.text!,
-            "password":passTextField.text!
-        ]
+        guard let url = URL(string: "http://fed-blog.herokuapp.com/api/v1/security/login") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
-        Alamofire.request(URLUser, method: .post, parameters: logParameters).responseJSON
-            {
-                response in
-                //printing response
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
                 print(response)
-                
-                //getting the json value from the server
-                if let result = response.result.value {
-                    let jsonData = result as! NSDictionary
-                    
-                    //if there is no error
-                    if(!(jsonData.value(forKey: "error") as! Bool)){
-                        
-                        //getting the user from response
-                        let user = jsonData.value(forKey: "user") as! NSDictionary
-                        
-                        //getting user values
-                        let userId = user.value(forKey: "id") as! Int
-                        let userName = user.value(forKey: "username") as! String
-                        let userEmail = user.value(forKey: "email") as! String
-                        let userPhone = user.value(forKey: "phone") as! String
-                        
-                        //saving user values to defaults
-                        self.defaultValues.set(userId, forKey: "userid")
-                        self.defaultValues.set(userName, forKey: "username")
-                        self.defaultValues.set(userEmail, forKey: "useremail")
-                        self.defaultValues.set(userPhone, forKey: "userphone")
-                        
-                        //switching the screen
-                        let profileViewController = self.storyboard?.instantiateViewController(withIdentifier: "ProfileViewcontroller") as! ProfileViewController
-                        self.navigationController?.pushViewController(profileViewController, animated: true)
-                        
-                        self.dismiss(animated: false, completion: nil)
-                    }else{
-                        //error message in case of invalid credential
-                        self.massegeLabel.text = "Invalid username or password"
-                    }
-                }
-        }
-        
-    }
-
-    
-    @IBAction func registerButton(_ sender: UIButton) {
-       
-        let regParameters: Parameters=[
-            "name":nameTextField.text!,
-            "password":passTextField.text!
-        ]
-        Alamofire.request(URLUser, method: .post, parameters: regParameters).responseJSON {
-            
-            response in
-            //printing response
-            print(response)
-            
-            if let result = response.result.value {
-                
-                //converting it as NSDictionary
-                let jsonData = result as! NSDictionary
-                
-                //displaying the message in label
-                self.massegeLabel.text = jsonData.value(forKey: "name") as! String?
             }
-    }
+            
+            guard let data = data else { return }
+            do {
+                let json: Any?
+                json = try JSONSerialization.jsonObject(with: data, options: [])
+                                print(json)
+                DispatchQueue.main.async {
+                    self.messageLabel.text = "User or password not valid"
+                }
+            } catch {
+                print(error)
+                //                self.titleLbl.text! = request.value(forHTTPHeaderField: "message")!
+                //                titleLbl.text = httpBody.value(forKey: "message"))
+            }
+            
+            
+            
+            }.resume()
+    
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let backButton = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: navigationController, action: nil)
-        navigationItem.leftBarButtonItem = backButton
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //if user is already logged in switching to profile screen
-        if defaultValues.string(forKey: "name") != nil{
-            let profileViewController = self.storyboard?.instantiateViewController(withIdentifier: "ProfileViewcontroller") as! ProfileViewController
-            self.navigationController?.pushViewController(profileViewController, animated: true)
-            
-        }
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
 
 
